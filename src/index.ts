b8f8d3097d91c03b02c2acb49d9c1daf2c651da8
pxgo <pengxiguaa@gmail.com>
2024-08-22 15:59:43 +0800

    fix: 修复依赖reactNative报错问题

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import { Platform } from "react-native";
-
 import ExpoEasyFsModule from "./ExpoEasyFsModule";
 
 export function copyFileToDownload(
   uri: string,
   filename: string,
 ): Promise<void> {
-  if (Platform.OS !== "android") {
-    throw new Error(`Not yet supported on ${Platform.OS}`);
-  }
   return ExpoEasyFsModule.copyFileToDownload(uri, filename);
 }
