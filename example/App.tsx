4312b4f072af01c40515b21cf2ecf78ca2510a8b
pxgo <pengxiguaa@gmail.com>
2024-12-06 09:44:26 +0800

    ref: modify example

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,6 @@
 import * as ExpoEasyFs from "expo-easy-fs";
 import * as fileSystem from "expo-file-system";
 import { StyleSheet, Text, View, StatusBar, Button } from "react-native";
-import {exists, getPaths, mkdir, remove} from "expo-easy-fs";
 
 export default function App() {
   return (
@@ -26,32 +25,6 @@ export default function App() {
             });
         }}
       />
-      <Button title={'mkdir'} onPress={() => {
-        getPaths().then(paths => {
-          const {downloads} = paths;
-          return mkdir(`${downloads}/KeChuangTest/aaa/bbb`)
-        })
-          .then((r) => {
-            console.log(`mkdir done: ${r}`);
-          }).catch(console.error);
-      }} />
-
-      <Button title={'mkdir & remove'} onPress={async () => {
-        try{
-          const paths = await getPaths();
-          const targetPath = `${paths.downloads}/KeChuangTest/111/222`;
-          await mkdir(targetPath);
-          console.log(`mkdir done: ${targetPath}`);
-          let isExists = await exists(targetPath);
-          console.log("exists: ", isExists);
-          await remove(targetPath);
-          console.log(`remove done: ${targetPath}`);
-          isExists = await exists(targetPath);
-          console.log("exists: ", isExists);
-        } catch(err) {
-          console.error(err);
-        }
-      }} />
     </View>
   );
 }
